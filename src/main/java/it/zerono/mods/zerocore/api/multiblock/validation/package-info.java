a8e42ff0e86ecb4c90da9cb2aa05573786000f9f
ZeroNoRyouki <zeronoryouki@gmail.com>
2016-07-21 23:31:58 +0200

    First commit

---
+++ src/main/java/it/zerono/mods/zerocore/api/multiblock/validation/package-info.java
@@ -0,0 +1,4 @@
+@API(apiVersion = "1.9.4-0.0.1", owner = "zerocore", provides = "zerocore|API|multiblock|validation")
+package it.zerono.mods.zerocore.api.multiblock.validation;
+
+import net.minecraftforge.fml.common.API;
