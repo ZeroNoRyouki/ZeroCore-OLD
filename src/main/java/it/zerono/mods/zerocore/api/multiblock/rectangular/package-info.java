a35ffb4cc88028cf54f91f810621f74fd60eac8c
ZeroNoRyouki <zeronoryouki@gmail.com>
2016-07-21 23:31:47 +0200

    First commit

---
+++ src/main/java/it/zerono/mods/zerocore/api/multiblock/rectangular/package-info.java
@@ -0,0 +1,4 @@
+@API(apiVersion = "1.9.4-0.0.1", owner = "zerocore", provides = "zerocore|API|multiblock|rectangular")
+package it.zerono.mods.zerocore.api.multiblock.rectangular;
+
+import net.minecraftforge.fml.common.API;
