05f102b7633ceb5b68e90e6b3db2fb9ebac54388
ZeroNoRyouki <zeronoryouki@gmail.com>
2016-07-21 22:56:49 +0200

    Fixed class-not-found while running on a server

--- src/main/java/zero/mods/zerocore/lib/network/ModTileEntityMessageHandlerClient.java
+++ src/main/java/zero/mods/zerocore/lib/network/ModTileEntityMessageHandlerClient.java
@@ -7,15 +7,16 @@ import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-@SideOnly(Side.CLIENT)
 public abstract class ModTileEntityMessageHandlerClient<MessageT extends ModTileEntityMessage>
         extends ModTileEntityMessageHandler<MessageT> {
 
+    @SideOnly(Side.CLIENT)
     @Override
     protected IThreadListener getThreadListener(MessageContext ctx) {
         return FMLClientHandler.instance().getClient();
     }
 
+    @SideOnly(Side.CLIENT)
     @Override
     protected World getWorld(MessageContext ctx) {
         return FMLClientHandler.instance().getClient().theWorld;
