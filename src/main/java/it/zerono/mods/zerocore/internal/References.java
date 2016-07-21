9ad18c56820bc9af156bc2283a568f13c11f5fe6
ZeroNoRyouki <zeronoryouki@gmail.com>
2016-07-21 23:22:10 +0200

    Changed main package name to it.zerono.mods

--- src/main/java/it/zerono/mods/zerocore/internal/References.java
+++ src/main/java/it/zerono/mods/zerocore/internal/References.java
@@ -3,12 +3,12 @@ package it.zerono.mods.zerocore.internal;
 public final class References {
 
     public static final String MOD_ID = "zerocore";
-    public static final String MOD_VERSION = "1.9.0-0.0.1";
-    public static final String MOD_MCVERSION = "1.9.0";
+    public static final String MOD_VERSION = "1.9.4-0.0.1";
+    public static final String MOD_MCVERSION = "1.9.4";
     public static final String MOD_DEPENDENCIES = "";
 
-    public static final String PROXY_COMMON = "zero.mods.zerocore.internal.common.CommonProxy";
-    public static final String PROXY_CLIENT = "zero.mods.zerocore.internal.client.ClientProxy";
+    public static final String PROXY_COMMON = "it.zerono.mods.zerocore.internal.common.CommonProxy";
+    public static final String PROXY_CLIENT = "it.zerono.mods.zerocore.internal.common.CommonProxy";
 
 
     private References() {
